409cc2cb0dbc5c8d102575c5d3224fe678ba461d
runar <runar@tuta.io>
2019-12-07 14:00:47 +0100

    added gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+  pathPrefix: "/posts",
+}
